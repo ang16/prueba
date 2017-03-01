d1e62defc6534fda585eb49dba970a4ae6281aa4
ang16 <angelgarcialopez@hotmail.com>
2017-03-02 00:54:43 +0100

    Add files via upload

--- RSA2/src/rsa2/Encriptacion.java
+++ RSA2/src/rsa2/Encriptacion.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,6 +12,7 @@ import java.util.ArrayList;
  * @author agarcia
  */
 public class Encriptacion {
+//ozzze
     long p,q,e,n,d,fn,c;
     int priv[]=new int[2],pub[]=new int[2];
     char[] alfabeto={' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z',' ','0','1','2','3','4','5','6','7','8','9','.',',',':',';'};
